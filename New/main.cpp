b9cee42aa3b5ba4cc2f1473fc655ecd577cdb1a0
Jasminezhang1 <fenzhang1211@gmail.com>
2024-03-19 21:30:27 -0400

    second time push new folder-not empty

---
+++ New/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Lab7
+//
+//  Created by Jasmine Zhang on 9/30/22.
+//
+#include"RectangleType.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int x0,y0,x1,y1;
+    cout<<"Rectangle APP!"<<endl;
+    cout<<"Please enter x0 and y0"<<endl;
+    cin>>x0;
+    cin>>y0;
+    cout<<"Please enter x1 and y1"<<endl;
+    cin>>x1;
+    cin>>y1;
+    
+    RectangleType myrectangle(x0,y0,x1,y1);
+    
+    cout<<" Rectangle information:"<<endl;
+    cout<<"Width: "<<myrectangle.getWidth()<<endl;
+    cout<<"Length: "<<myrectangle.getLength()<<endl;
+    cout<<" Perimeter:" <<myrectangle.getPerimeter()<<endl;
+    cout<<"Area: "<<myrectangle.getArea()<<endl;
+    
+    return 0;
+}
